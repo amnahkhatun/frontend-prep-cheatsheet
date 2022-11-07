2ff6624b21c6e1d3c8ecb4eb4f9859cba1965b83
Amnah Khatun <amna.khatun1605@gmail.com>
2022-11-07 10:57:35 +0530

    updated questions

--- questions.js
+++ questions.js
@@ -1,9 +1,9 @@
+##PAYTM
+
 // problem: given array = [1,2,3,4,5,6]
 // print all ordered pairs that matches the sum n.
 // n = 8
 // so output is [[2,6],[3,5],[5,3],[6,2]]
-
-//paytm
 // let arr = [1, 2, 3, 4, 5, 6];
 let arr = [-1, 1, 2, 3, 4, 5, 6, 9];
 
@@ -34,49 +34,39 @@ function orderedArray(arr) {
   }
 }
 // orderedArray1(arr);
-
+```
 var x = "x";
 
 function findName() {
   console.log(x); //undefined
   var x = "b";
 }
-// findName();
-
+findName();
+```
+---
+```
 var obj = {
   a: 7,
-  b: () => console.log(this.a, this), // window, {window object}
+  b: () => console.log(this.a, this), //undefined, Window
   c: function () {
-    console.log(this.a, this); // 7, {}
+    console.log(this.a, this); // 7, {a: 7, b: ƒ, c: ƒ}
   }
 };
-// obj.b();
-// obj.c();
-
-///////////
+obj.b();
+obj.c();
+```
+---
+```
 setTimeout(() => console.log("1"), 0);
 setTimeout(() => console.log("2"), 500);
 Promise.resolve("3").then(val => console.log(val));
 console.log("4");
 
-//4,3,1,2
-
-//reduce
-
-Array.prototype.myReduce = function (callback, value) {
-  let a = this;
-  a.forEach(item => {
-    value = value !== undefined ? callback(value, item) : item;
-  });
-  return value;
-};
-let x1 = [1, 2, 3].myReduce(function (a, b) {
-  return a + b;
-});
-// console.log(x1);
-
-// add(1, 2) || add(1)(2)
-//  => outputs the sum of all params
+// O/P : 4,3,1,2
+```
+---
+```
+add(1, 2) || add(1)(2)
 
 function add(x, y) {
   if (y !== undefined) {
@@ -87,31 +77,30 @@ function add(x, y) {
     };
   }
 }
-
-// add2(1, 2, 3, ...) || add2(1)(2)(3)...
-// => returns the sum of all params
-
+```
+---
+```
 function add2(...arr) {
-  // console.log(arr);
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
   return sum;
 }
-
-// console.log(add2(1, 2, 3, 4));
-
-// const add3 = (a) => (b) => (b ? add3(a + b) : a);
+console.log(add2(1, 2, 3, 4)); //10
+```
+---
+```
 function add3(a) {
   return function (b) {
     return b == null ? a : add3(a + b);
   };
 }
-console.log(add3(2)(3)(1)(4)());
-
-//nium
-
+add3(2)(3)(1)(4)(); //10
+```
+---
+##NIUM
+```
 let obj1 = {
   fname: "amnah",
   lname: "khatun",
@@ -124,23 +113,27 @@ let obj2 = { ...obj1 }; //shallow copy
 
 obj2.fname = "elon";
 obj2.address.city = "new york";
-// console.log(obj1, "obj1"); // { fname: 'amnah', lname: 'khatun', address: { city: 'new york' } }
-// console.log(obj2, "obj2"); // { fname: 'elon', lname: 'khatun', address: { city: 'new york' } }
+console.log(obj1, "obj1"); // { fname: 'amnah', lname: 'khatun', address: { city: 'new york' } }
+console.log(obj2, "obj2"); // { fname: 'elon', lname: 'khatun', address: { city: 'new york' } }
 
-//aptech
+let obj2 = obj1;
+console.log(obj1, "obj1"); // { fname: 'elon', lname: 'khatun', address: { city: 'new york' } }
+console.log(obj2, "obj2"); // { fname: 'elon', lname: 'khatun', address: { city: 'new york' } }
 
+```
+##APTECH
+```
 (function () {
-  //b = 3;
-  //var a = b;
-
   var a = (b = 3);
 
-  console.log(a, b);
+  console.log(a, b); // 3,3
 })();
 
-// console.log("a defined? " + typeof a); //undefined
-// console.log("b defined? " + typeof b); //number
-
+console.log("a defined? " + typeof a); //undefined
+console.log("b defined? " + typeof b); //number
+```
+---
+```
 var foo = "xyz";
 
 var myObject = {
@@ -156,61 +149,51 @@ var myObject = {
     })();
   }
 };
-// myObject.func();
+myObject.func();
+```
+---
 
-/* let c = 20;
-function outer (){
-  let a = 10;
-  return function inner(){
-  let b =100;
-    console.log(a,b,c)
+##Byjus
+```
+let filterNonUnique = arr => {
+  let mySet = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    if (!mySet.has(arr[i])) {
+      mySet.add(arr[i]);
+    } else {
+      mySet.delete(arr[i]);
+    }
   }
-}
+  return mySet;
+};
 
-let abc = outer();
-abc();
+console.log(filterNonUnique(["a", "c", "b", "d", "c", "b", "e"])); //{'a', 'd', 'e'}
+```
+---
+```
+//Using map
 
-outer()(); */
-
-let str = "amnah";
-let str1 = str.split("");
-console.log(str1.reverse().join(""));
-
-//byjus
-
-console.log(filterNonUnique(["a", "c", "b", "d", "c", "b", "e"])); // ["a", "d", "e"]
-console.log(filterNonUnique(["a", "c", "b", "d"])); // ["a", "c", "b", "d"]
-
-// let filterNonUnique = arr => {
-//   let mySet = new Set();
-//   for (let i = 0; i < arr.length; i++) {
-//     if (!mySet.has(arr[i])) {
-//       mySet.add(arr[i]);
-//     } else {
-//       mySet.delete(arr[i]);
-//     }
-//   }
-//   return mySet;
-// };
-
-// let filterNonUnique = arr => {
-//   let myMap = new Map();
-//   for (let i = 0; i < arr.length; i++) {
-//     if (!myMap.has(arr[i])) {
-//       myMap.set(arr[i], 1);
-//     } else {
-//       myMap.set(arr[i], myMap.get(arr[i]) + 1);
-//     }
-//   }
-//   let temp = [];
-//   myMap.forEach((value, key) => {
-//     if (value === 1) {
-//       temp.push(key);
-//     }
-//   });
-//   return temp;
-// };
-
+let filterNonUnique = arr => {
+  let myMap = new Map();
+  for (let i = 0; i < arr.length; i++) {
+    if (!myMap.has(arr[i])) {
+      myMap.set(arr[i], 1);
+    } else {
+      myMap.set(arr[i], myMap.get(arr[i]) + 1);
+    }
+  }
+  let temp = [];
+  myMap.forEach((value, key) => {
+    if (value === 1) {
+      temp.push(key);
+    }
+  });
+  return temp;
+};
+```
+---
+  
+```
 //replace space
 replace(/ /g, "");
 
@@ -231,21 +214,24 @@ function isPalindrome(str) {
 console.log(isPalindrome("level")); // logs 'true'
 console.log(isPalindrome("levels")); // logs 'false'
 console.log(isPalindrome("A car, a man, a maraca")); // logs 'true'
-
-// const obj1 = { color: "white", age: 25, weight: true };
-// const obj2 = { color: "white", weight: true };
-// const obj3 = { color: "white", age: 26, weight: true };
-// console.log(matches(obj1, obj2)); // true
-// console.log(matches(obj2, obj1)); // false
-// console.log(matches(obj3, obj1)); // false
-// console.log(matches(obj3, obj2)); // true
-
+```
+---
+```
 function matches(obj1, obj2) {
   for (let [key, value] of Object.entries(obj1)) {
     console.log(key);
   }
 }
-
+const obj1 = { color: "white", age: 25, weight: true };
+const obj2 = { color: "white", weight: true };
+const obj3 = { color: "white", age: 26, weight: true };
+console.log(matches(obj1, obj2)); // true
+console.log(matches(obj2, obj1)); // false
+console.log(matches(obj3, obj1)); // false
+console.log(matches(obj3, obj2)); // true
+```
+---
+```
 function foo() {
   let a = (b = 0);
   a++;
@@ -255,7 +241,9 @@ function foo() {
 foo();
 console.log(typeof a); //undefined
 console.log(typeof b); //number
-
+```
+---
+```
 var Employee = {
   company: "xyz"
 };
@@ -263,11 +251,16 @@ var emp1 = Object.create(Employee);
 delete emp1.company;
 console.log(emp1.company); //xyz
 
-for (var i = 0; i < 5; i++) {
-  setTimeout(console.log(i), 0);
-}
-//0,1,2,3,4
+var emp1 = Employee;
+delete emp1.company;
+console.log(emp1.company); //undefined
 
+var emp1 = {...Employee};
+delete emp1.company;
+console.log(emp1.company) //undefined
+```
+---
+```
 console.log(1 + "2" + "2"); // 122
 console.log(1 + +"2" + "2"); // 32
 console.log(1 + -"1" + "2"); // 02
@@ -279,9 +272,10 @@ console.log("0 || 1 = " + (0 || 1)); //1
 console.log("1 || 2 = " + (1 || 2)); //1
 console.log("0 && 1 = " + (0 && 1)); //0
 console.log("1 && 2 = " + (1 && 2)); //2
+```
 
-//Nium
-
+##Nium
+```
 var structure2 = {
   name: "alpha",
   array: [
@@ -335,21 +329,25 @@ console.log(getNames(structure2));
 
 // expected result: [ 'alpha', 'beta', 'gamma', 'delta', 'epsilon' ]
 // expected result: [ 'alpha', 'beta', 'alpha1', 'beta1', 'gamma1', 'delta1', 'epsilon1','gamma', 'delta', 'epsilon' ]
+```
+---
+```
+let obj1 = {
+  fname: "amnah",
+  khatun: "khatun",
+  address: {
+    city: "jsr"
+  }
+};
 
-// let obj1 = {
-//   fname: "amnah",
-//   khatun: "khatun",
-//   address: {
-//     city: "jsr"
-//   }
-// };
+let obj2 = { ...obj1 };
 
-// let obj2 = { ...obj1 };
-
-// obj2.address.city = "mumbai";
-// console.log(obj1, "obj1");
-// console.log(obj2, "obj2");
-
+obj2.address.city = "mumbai";
+console.log(obj1, "obj1"); // both obj.address.city will change
+console.log(obj2, "obj2");
+```
+---
+```
 function delay(time) {
   let myPromise = new Promise(function (resolve) {
     setTimeout(resolve, time);
@@ -361,8 +359,8 @@ function delay(time) {
 var callback = () => alert("runs after 3 seconds");
 
 delay(3000).then(callback);
-
-// Zapr 1st round
+```
+## Zapr 1st round
 let obj1 = {
   fname: "amnah"
 };
